afdcb3cbce530c978272b2af5a34dbb29cba8157
Barros <edersombarros@hotmail.com>
2022-08-29 15:23:10 -0400

    --

---
+++ Fundamentos/templaytString.js
@@ -0,0 +1,11 @@
+const nome ='Rebeca'
+const concatenacao = 'Olá' + nome + '!'
+const tamplate = `
+    olá
+    ${nome}!`
+     console.log(concatenacao,tamplate);
+
+     console.log(`1 + 1 = ${1+1}`)
+
+     const up = texto => texto.toUpperCase()
+     console.log(`Ei.. ${up('cuidado')}`);
