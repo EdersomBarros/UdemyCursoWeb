3bf6cc3952fe6438214036669c44a6f6000b5a06
Barros <edersombarros@hotmail.com>
2022-09-09 16:04:06 -0400

    aula construtor

---
+++ Funcao/tiposDeclaracao.js
@@ -0,0 +1,15 @@
+//function declaration
+console.log(soma(3,4));
+function soma(x, y) {
+    return x + y
+}
+//function expression
+const sub = function (x, y) {
+    return x - y
+}
+console.log(sub(3,9));
+//NAMED function expression
+const mult = function mult(x,y){
+    return x * y
+}
+console.log(mult(3,6));
