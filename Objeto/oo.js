d35a395bfb7c389c2d2920d8e03110b07921ac05
Barros <edersombarros@hotmail.com>
2022-10-03 17:12:45 -0400

    AULA OBJETOS

---
+++ Objeto/oo.js
@@ -0,0 +1,20 @@
+// código não executável
+//procedural
+processamento(valor1, valor2, valor3)
+
+// oo tem dados  e dentro desses dados têm funções
+objeto ={
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+objeto.processamento()
+/*princípios importantes: 
+1- Abstração(peso, altura, liga/desliga)
+2- encapsulamento
+3- herança (prototype)
+4- polimorfismo -  multiplas formas
+*/
