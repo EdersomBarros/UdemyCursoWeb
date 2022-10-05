b65e59974994f31f3f965c3626b6f43bebd806f3
Barros <edersombarros@hotmail.com>
2022-10-05 16:00:19 -0400

    aula array

---
+++ Array/MetodosImportantes.js
@@ -0,0 +1,24 @@
+const pilotos =['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop()//massa quebrou o carro!
+console.log(pilotos);
+pilotos.push('Verstappen')
+console.log(pilotos);
+pilotos.shift()// remove o primeiro
+console.log(pilotos);
+
+pilotos.unshift('Hamilton')
+console.log(pilotos); //inclui no primeiro
+
+// splice pode adicionar e remover elementos
+//adicionar
+pilotos.splice(2,0,'Bottas', 'Massa')
+console.log(pilotos);
+//remover
+pilotos.splice(3,1)//massa quebrou novamente
+console.log(pilotos);
+
+const algunsPiloto1 = pilotos.slice(2)// novo array
+console.log(algunsPiloto1);
+
+const algunsPiloto2 = pilotos.slice(1,4)
+console.log(algunsPiloto2);
